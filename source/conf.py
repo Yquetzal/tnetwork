ebc8d085fffedd275f1770dedc8daba290b14364
Yquetzal <remy.cazabet@gmail.com>
2019-03-11 17:33:14 +0100

    First version of the complete documentation

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 import sphinx_rtd_theme
+from mock import Mock as MagicMock
 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['numpy', 'scipy', 'networkx', 'pandas','bokeh']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
